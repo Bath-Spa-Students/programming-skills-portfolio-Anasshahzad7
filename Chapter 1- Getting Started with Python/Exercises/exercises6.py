606351dca8e3533969b10c85155ddaa33a12c99f
Anasshahzad7 <144208579+Anasshahzad7@users.noreply.github.com>
2023-10-24 10:10:00 +0400

    Exercises

---
+++ Chapter 1- Getting Started with Python/Exercises/exercises6.py
@@ -0,0 +1,13 @@
+# Homogenous List / Phyton Supports Hetrogenous List other than C , Java
+
+names =[ "Anas" , "Bathspa" , "Code Lab "1 ]
+print(names)
+
+# Hetrogenous List
+Student =["Anas Shahzad" , "234" , "3.890"]
+print(Student)
+
+
+#negative numbers and positive number start
+numbers = [6,97,8]
+print(numbers[3])
---
+++ Chapter 1- Getting Started with Python/Exercises/practice5.py
@@ -0,0 +1,8 @@
+#write four strings in different variables and print the output as one string (String concantenation)
+name= "Najwan"
+course= "cybersecurity"
+nationality= "INDIAN"
+School= "SIS"
+
+Result = (name + "" +  course + "" +  ""  +nationality + "" + School)
+print(Result)

7e9393eee405750670792b7611f50ebd637dbe3c
Anasshahzad7 <144208579+Anasshahzad7@users.noreply.github.com>
2023-10-24 10:47:42 +0400

    chapter 3 exercises

---
+++ Chapter 3- Data Structures/Exercises/exercise3.py
@@ -0,0 +1,5 @@
+#Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
+transportation = ["car", "lambo"]
+
+print("I go to university using a {}".format(transportation[0]))
+print("I would like to own a {}".format(transportation[1]))
